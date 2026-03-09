d3efd36a0db179dfc9a52304671ec31620710788
Ajay Rana <ajurana2021@gmail.com>
2026-03-09 19:59:40 +0530

    fixing server.js

--- backend/server.js
+++ backend/server.js
@@ -15,29 +15,24 @@ connectDB();
 connectCloudinary();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
     origin: [
-      "https://prescripto-admin12.vercel.app",
+      "https://prescripto-admin.vercel.app",
       "https://prescripto-frontend.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "token"],
     credentials: true
   })
 );
 
-app.options("*", cors());
-
-// Routes
 app.use("/api/admin", adminRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
-  res.status(200).send("API Working");
+  res.send("API Working");
 });
 
 export default app;
